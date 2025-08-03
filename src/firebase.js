67f58c5eb06aabfecdf1f4a8b8046e576722069b
keito-mh <keito@meta-heroes.io>
2025-08-03 15:28:07 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  // あなたのFirebase設定
+  apiKey: "AIzaSyAw1foKE0J7Y5ns-zQv4a7FSKMiUQGnvOc",
+  authDomain: "attendance-app-137a2.firebaseapp.com",
+  projectId: "attendance-app-137a2",
+  storageBucket: "attendance-app-137a2.firebasestorage.app",
+  messagingSenderId: "232882299054",
+  appId: "1:232882299054:web:6b9d73f6b47fb6d1a38d3f"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export default app;
